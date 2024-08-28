342853af7c80a53112068fece0b5b99196d121fd
aishwarya <aishwaryatadala0459@gmail.com>
2024-08-28 16:16:30 +0530

    sep

--- src/Lab002.py
+++
@@ -1,3 +0,0 @@
-print("hello,world!")
-print("hello","world",123,5.164,"aishwarya","true", sep="|",end=" ")
-print("aishwarya")
--- src/ex_08082024/Lab001.py
+++
@@ -1,4 +0,0 @@
-# This is comment -which doesn't run.
-print("Hello Aishwarya,welcome to learning Python")
-print(2+2)
-print(2-2)
